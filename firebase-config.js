a9c0064df2dcfd8b342e19beba1b7aed28e33de0
minaamagdi7-source <minaamagdi7@gmail.com>
2025-09-07 10:31:12 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,25 @@
-<!-- firebase-config.js -->
+<!-- Firebase SDKs (Compat) -->
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-storage-compat.js"></script>
 
 <script>
-const firebaseConfig = {
+  // إعدادات Firebase
+  const firebaseConfig = {
     apiKey: "AIzaSyDKuEGoUAcbKCPRNQtYvKfz4WPTGQUHR0w",
     authDomain: "idid-store.firebaseapp.com",
     projectId: "idid-store",
     storageBucket: "idid-store.appspot.com",
     messagingSenderId: "1024069736444",
     appId: "1:1024069736444:web:1eba37134473a9474b4479"
-};
+  };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const storage = firebase.storage();
-const auth = firebase.auth();
+  // تهيئة Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // اختصارات للوظائف
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+  const storage = firebase.storage();
 </script>
